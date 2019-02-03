743987c1f4c7d6e4fe26b2b4ca62a1f33d967118
GeneLil <evgeniy.lilik@gmail.com>
2019-02-04 00:22:46 +0200

    Added typescript to project

--- main.js
+++ main.js
@@ -1,33 +1,16 @@
-const colorsArray = ["rgb(42, 96, 179)", "rgb(252, 229, 52)", "rgb(83, 83, 83)"]
-const type = "T-Shirt"
-const productImage = "./img/tshirt.jpg"
-const price = "5.00"
-const currency = "\u20AC"
-
-const renderCard = () => {
-    const container = document.getElementById("main")
-    const card = document.createElement("div")
-    card.innerHTML = `
-    <div class="card">
-        <div class='cardHeader'>
-            <div class='dropDown'>Size
-                <span class='arrow'/>
-            </div>
-            <div class='colors'>${colorsArray.map(color => 
-                `<span class='color' style='background-color:${color}'></span>`).join("")}
-            </div>
-        </div>
-        <div class="cardBody">
-            <img src=${productImage} />
-        </div>
-        <div class="cardFooter">
-            <span>${type}</span>
-            <span class="price">${price + currency}</span>
-        </div>
-    </div>`
-    container.appendChild(card)
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    renderCard()
-})
+var colorsArray = ["rgb(42, 96, 179)", "rgb(252, 229, 52)", "rgb(83, 83, 83)"];
+var type = "T-Shirt";
+var productImage = "./img/tshirt.jpg";
+var price = "5.00";
+var currency = "\u20AC";
+var renderCard = function () {
+    var container = document.getElementById("main");
+    var card = document.createElement("div");
+    card.innerHTML = "\n    <div class=\"card\">\n        <div class='cardHeader'>\n            <div class='dropDown'>Size\n                <span class='arrow'/>\n            </div>\n            <div class='colors'>" + colorsArray.map(function (color) {
+        return "<span class='color' style='background-color:" + color + "'></span>";
+    }).join("") + "\n            </div>\n        </div>\n        <div class=\"cardBody\">\n            <img src=" + productImage + " />\n        </div>\n        <div class=\"cardFooter\">\n            <span>" + type + "</span>\n            <span class=\"price\">" + (price + currency) + "</span>\n        </div>\n    </div>";
+    container.appendChild(card);
+};
+document.addEventListener("DOMContentLoaded", function () {
+    renderCard();
+});
---
+++ main.ts
@@ -0,0 +1,33 @@
+const colorsArray = ["rgb(42, 96, 179)", "rgb(252, 229, 52)", "rgb(83, 83, 83)"]
+const type = "T-Shirt"
+const productImage = "./img/tshirt.jpg"
+const price = "5.00"
+const currency = "\u20AC"
+
+const renderCard = () => {
+    const container = document.getElementById("main")
+    const card = document.createElement("div")
+    card.innerHTML = `
+    <div class="card">
+        <div class='cardHeader'>
+            <div class='dropDown'>Size
+                <span class='arrow'/>
+            </div>
+            <div class='colors'>${colorsArray.map(color => 
+                `<span class='color' style='background-color:${color}'></span>`).join("")}
+            </div>
+        </div>
+        <div class="cardBody">
+            <img src=${productImage} />
+        </div>
+        <div class="cardFooter">
+            <span>${type}</span>
+            <span class="price">${price + currency}</span>
+        </div>
+    </div>`
+    container.appendChild(card)
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    renderCard()
+})
