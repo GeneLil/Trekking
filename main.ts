93da6d76befbdce5ac703d9600aaa82fcb066138
GeneLil <evgeniy.lilik@gmail.com>
2019-02-07 00:28:23 +0200

    Added categories section and counter
    moved render from ajax call function, improved ts watch script

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let mainContainer: HTMLElement | null
+
 interface IProduct {
     colors: string[]
     type: string
@@ -7,10 +9,10 @@ interface IProduct {
 }
 
 const renderCard = (product: IProduct) => {
-    const container = document.getElementById("main")
     const card = document.createElement("div")
+    const cardsWrapper = document.querySelector(".cards-container")
     card.classList.add("card")
-    card.innerHTML = `
+    card.innerHTML = `      
         <div class='cardHeader'>
             <div class='dropDown'>Size
                 <span class='arrow'/>
@@ -25,24 +27,93 @@ const renderCard = (product: IProduct) => {
         <div class="cardFooter">
             <span>${product.type}</span>
             <span class="price">${product.price + product.currency}</span>
-        </div>`
-    container.appendChild(card)
+        </div>
+       `
+    if (cardsWrapper) {
+        cardsWrapper.appendChild(card)
+    }
 }
 
-const getProducts = (): void => {
-    const productsQuery = new XMLHttpRequest()
-    productsQuery.open("GET", "products.json", true)
-    productsQuery.send()
-
-    productsQuery.onreadystatechange = () => {
-        if (productsQuery.readyState !== 4) {
-            return
+const getProducts = () => new Promise((resolve, reject) => {
+        const productsQuery = new XMLHttpRequest()
+        productsQuery.open("GET", "products.json", true)
+        productsQuery.send()
+        productsQuery.onreadystatechange = () => {
+            if (productsQuery.readyState !== 4) {
+                return
+            }
+            if (productsQuery.status !== 200) {
+                reject(productsQuery.statusText)
+            } else {
+                resolve(productsQuery.responseText)
+            }
         }
-        const products: IProduct[] = JSON.parse(productsQuery.responseText)
-        products.map(renderCard)
+    })
+
+const renderCategorySelectionBlock = () => {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("categorySelectionWrapper")
+    wrapper.innerHTML = `
+        <div class="checkboxes">          
+            <input id="menCategory" type="checkbox" />
+            <label for="menCategory">T-Shirt</label>           
+            <input id="womenCategory" type="checkbox" />
+            <label for="womenCategory">Backpack</label>
+            <input id="childCategory" type="checkbox" />
+            <label for="childCategory">Hoodie</label>
+        </div>
+        <button class="categories-button">See all products</button>
+    `
+    if (mainContainer) {
+        mainContainer.appendChild(wrapper)
+    }
+}
+
+const renderCategoryBlock = () => {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("category-block")
+    wrapper.innerHTML = `
+        <span>T-Shirt</span>
+        <hr class="category-divider" />
+    `
+    if (mainContainer) {
+        mainContainer.appendChild(wrapper)
+    }
+}
+
+const renderCardsContainer = () => {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("cards-container")
+    if (mainContainer) {
+        mainContainer.appendChild(wrapper)
+    }
+}
+
+const renderCounter = () => {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("counter-block")
+    wrapper.innerHTML = `
+        <div class="numbers">1 / 5</div>
+        <div class="arrows">
+            <span class="counter-arrow left"></span>
+            <span class="counter-arrow right"></span>
+        </div>
+    `
+    if (mainContainer) {
+        mainContainer.appendChild(wrapper)
     }
 }
 
 document.addEventListener("DOMContentLoaded",  () => {
-    getProducts()
+    mainContainer = document.getElementById("main")
+
+    renderCategorySelectionBlock()
+    renderCategoryBlock()
+    renderCounter()
+    renderCardsContainer()
+
+    getProducts().then((response: any) => {
+        const products: IProduct[] = JSON.parse(response)
+        products.map(renderCard)
+    }).catch(e => console.log(e.message))
 })
